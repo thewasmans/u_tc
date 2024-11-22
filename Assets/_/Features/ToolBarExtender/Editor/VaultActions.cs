b58f4e08f10dd418f8a0e02ef5fb3be4887dcc17
thewasmans <thewasmans@gmail.Com>
2024-11-22 14:58:19 +0100

    10. Add to dropdown "Build Addressable"
    Ajouter au dropdown "Addressable" avec un bouton "build addressable" qui target le new build de l'addressable window

--- Assets/_/Features/ToolBarExtender/Editor/VaultActions.cs
+++ Assets/_/Features/ToolBarExtender/Editor/VaultActions.cs
@@ -1,27 +1,74 @@
+using System;
 using System.Linq;
+using Level.Editor;
 using Misc.Editor;
 using Paps.UnityToolbarExtenderUIToolkit;
+using UnityEngine;
 using UnityEngine.UIElements;
+
+public static class VaulActionsConst
+{
+    public static string ADDRESSABLE = "Addressable";
+    public static string FEATURE = "Features";
+    public static Action<int> onChanged;
+}
+
+public struct VaultAction
+{
+    public string Property;
+    public string Text;
+    public Action Action;
+}
+
+public static class VaultActions
+{
+    public static VaultAction[] Actions =
+    {
+        new VaultAction()
+        {
+            Property = VaulActionsConst.FEATURE,
+            Text = "New Feature",
+            Action = FeatureCreator.ShowWindow
+        },
+        new VaultAction()
+        {
+            Property = VaulActionsConst.ADDRESSABLE,
+            Text = "Build Addressable",
+            Action = () => Debug.LogError("New Addresaable Window not implemented")
+        },
+    };
+}
 
 namespace ToolBarExtender.Editor
 {
-    [MainToolbarElement(id: "LevelSelector", ToolbarAlign.Right, order:0)]
+    [MainToolbarElement(id: "LevelSelector", ToolbarAlign.Right, order: 0)]
     public class VaultActionsDropdown : DropdownField
     {
         public void InitializeElement()
         {
-            choices = new string[]{"Features"}.ToList();
+            choices = VaultActions.Actions.Select(a => a.Property).ToList();
+            this.RegisterValueChangedCallback(e => VaulActionsConst.onChanged.Invoke(index));
             value = choices.FirstOrDefault();
         }
     }
 
-    [MainToolbarElement(id: "StyledButton", ToolbarAlign.Right, order:1)]
+    [MainToolbarElement(id: "LevelSelector", ToolbarAlign.Right, order: 1)]
     public class VaultActionButton : Button
     {
-        public VaultActionButton()
+        protected VaultAction _action;
+
+        protected void InitializeElement()
         {
-            text = "New Feature";
-            clicked += () => FeatureCreator.ShowWindow();
+            VaulActionsConst.onChanged += OnChange;
+            text = VaultActions.Actions.Select(a => a.Property).ToList().FirstOrDefault();
+        }
+
+        public void OnChange(int indexDropdown)
+        {
+            clicked -= _action.Action;
+            _action = VaultActions.Actions[indexDropdown];
+            text = _action.Text;
+            clicked += _action.Action;
         }
     }
 }
